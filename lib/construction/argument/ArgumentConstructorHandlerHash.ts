3e60a409f39f29de1fce4468315548d949a7a143
Ruben Taelman <ruben.taelman@ugent.be>
2023-05-23 13:50:17 +0200

    Revert "Allow hash keys to be URIs that dereference to a string"
    This reverts commit b35b725f266c93ae8cee63f54175b3fef970d562.

--- lib/construction/argument/ArgumentConstructorHandlerHash.ts
+++ lib/construction/argument/ArgumentConstructorHandlerHash.ts
@@ -29,16 +29,14 @@ export class ArgumentConstructorHandlerHash implements IArgumentConstructorHandl
       if (!entry.property.key) {
         throw new ErrorResourcesContext(`Missing key in fields entry`, { entry, argument });
       }
-
-      const key = await argsCreator.getArgumentValues(entry.properties.key, settings);
-      if (typeof key !== 'string') {
-        throw new ErrorResourcesContext(`Illegal non-string key (${entry.property.key.value} as ${entry.property.key.type}) in fields entry`, { entry, argument });
+      if (entry.property.key.type !== 'Literal') {
+        throw new ErrorResourcesContext(`Illegal non-literal key (${entry.property.key.value} as ${entry.property.key.type}) in fields entry`, { entry, argument });
       }
 
       // Recursively get value arg value
       if (entry.property.value) {
         const subValue = await argsCreator.getArgumentValues(entry.properties.value, settings);
-        return { key, value: subValue };
+        return { key: entry.property.key.value, value: subValue };
       }
 
       // Ignore cases where value may not be set, because params may be optional
